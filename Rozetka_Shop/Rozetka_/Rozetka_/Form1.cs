e4bb43604e063428e47837344cf9163085beec86
Max Babichevskyi <100165364+MaxBabichevskiy@users.noreply.github.com>
2023-03-23 01:54:51 +0300

    Add files via upload

---
+++ Rozetka_Shop/Rozetka_/Rozetka_/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rozetka_
+{
+    public partial class Form1 : Form
+    {
+    public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+        private void LoadData(){
+            using (SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=Rozetka;Integrated Security=True"))
+            {
+                connection.Open();
+                string sql = "SELECT * FROM Users";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        lstUsers.Items.Clear();
+                        while (reader.Read())
+                        {
+                            ListViewItem item = new ListViewItem(reader["Id"].ToString());
+                            item.SubItems.Add(reader["Name"].ToString());
+                            item.SubItems.Add(reader["Email"].ToString());
+                            item.SubItems.Add(reader["Password"].ToString());
+                            item.SubItems.Add(reader["Phone"].ToString());
+                            item.SubItems.Add(reader["Address"].ToString());
+                            lstUsers.Items.Add(item);
+                        }
+                    }
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e){           
+                using (SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=Rozetka;Integrated Security=True"))
+                {
+                    connection.Open();
+                    string sql = "INSERT INTO Users (Name, Email, Password, Phone, Address) VALUES (@Name, @Email, @Password, @Phone, @Address)";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@Name", txtName.Text);
+                        command.Parameters.AddWithValue("@Email", txtEmail.Text);
+                        command.Parameters.AddWithValue("@Password", txtPassword.Text);
+                        command.Parameters.AddWithValue("@Phone", txtPhone.Text);
+                        command.Parameters.AddWithValue("@Address", txtAddress.Text);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            LoadData();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=Rozetka;Integrated Security=True"))
+            {
+                connection.Open();
+                string sql = "UPDATE Users SET Name=@Name, Email=@Email, Password=@Password, Phone=@Phone, Address=@Address WHERE Id=@Id";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", txtId.Text);
+                    command.Parameters.AddWithValue("@Name", txtName.Text);
+                    command.Parameters.AddWithValue("@Email", txtEmail.Text);
+                    command.Parameters.AddWithValue("@Password", txtPassword.Text);
+                    command.Parameters.AddWithValue("@Phone", txtPhone.Text);
+                    command.Parameters.AddWithValue("@Address", txtAddress.Text);
+                    command.ExecuteNonQuery();
+                }
+            }
+            LoadData();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=Rozetka;Integrated Security=True"))
+            {
+                connection.Open();
+                string sql = "DELETE FROM Users WHERE Id=@Id";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", txtId.Text);
+                    command.ExecuteNonQuery();
+                }
+            }
+            LoadData();
+        }
+
+    }
+}
---
+++ Rozetka_Shop/Rozetka_/Rozetka_/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Rozetka_")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rozetka_")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("75ac5b9b-3c84-4275-8d8a-36f259c0dc91")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
